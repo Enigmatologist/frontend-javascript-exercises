979c69e53ee0e19d0e37aa977d586175f7fd36f4
Toby T Castro <tc6586@gmail.com>
2015-02-23 17:36:21 +0000

    Added Array Definition Code

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,13 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(a,b,c,d){
+  var arr = [a,b,c,d];
+  return arr;
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(arr){
+  var newArr = [];
+  var first = arr[0];
+  var last = arr[arr.length-1];
+  newArr.push(first,last);
+
+  return newArr;
+};
