cd2ff2e1c0df398eab6b5d35419db01447acb856
Toby T Castro <tc6586@gmail.com>
2015-02-24 01:17:35 +0000

    Added code for object exercises

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,31 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents){
+    var newObj = {
+      title: courseTitle,
+      duration: courseDuration,
+      students: courseStudents
+    };
 
-module.exports.addProperty = undefined;
+    return newObj;
+};
 
-module.exports.formLetter = undefined;
+module.exports.addProperty = function(object, newProp, newValue){
+  if(!object.hasOwnProperty(newProp)){
+    object[newProp] = newValue;
+  }
 
-module.exports.canIGet = undefined;
+  return object;
+};
+
+module.exports.formLetter = function(object){
+  return "Hello " + object.recipient + ",\n\n" + object.msg + "\n\n" + "Sincerely,\n" + object.sender;
+};
+
+module.exports.canIGet = function(item, money){
+  var itemList = {
+    'MacBook Air': 999,
+    'MacBook Pro': 1299,
+    'Mac Pro': 2499,
+    'Apple Sticker': 1
+  };
+return money >= itemList[item];
+};
